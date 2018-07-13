1a21772493995bf7097a1f3511a890c03c1410a8
BEAU WITKA <bwitka@gmail.com>
2018-07-13 20:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var name = "Joe"
-var height = "74"
+var name = "Joe";
+var height = "74";
  
+ if (name === "Joe") {
+    console.log("The name is correct")
+ } else {
+    console.log("The name is supposed to be 'Joe'")
+ }
 // Don't worry about this
 module.exports = { name, height
 }
