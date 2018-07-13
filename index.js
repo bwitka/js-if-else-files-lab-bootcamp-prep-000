c6a5d024cf1ddd8a04923e07a6dd499287262682
BEAU WITKA <bwitka@gmail.com>
2018-07-13 20:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var height = 70
     console.log("The name is supposed to be 'Susan'")
  }
  
- if (height === "70") {
+ if (height === 70) {
     console.log("The height is correct")
  } else {
     console.log("The height is supposed to be 70")
